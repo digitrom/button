0ba5798d654f6589fec694d90568588486368302
digitrom <roma.bobrovski@gmail.com>
2023-01-28 01:07:01 +0300

    Commit - 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {MouseEvent} from 'react';
 import './App.css';
 
+
+
 function App() {
+  // const myFirstSubscriber = (e:MouseEvent<HTMLButtonElement>) => {
+  //     console.log("Hello Roma!") }
+  //   const mySecondSubscriber = (e:MouseEvent<HTMLButtonElement>) => {
+  //     console.log("Hello Ivan!") }
+  //   const names = [
+  //       { name1: "Roma", name2: "Ivan"}
+  //   ]
+  //
+  //   const onClickHandler = (name: string) => {
+  //        console.log(name)
+  //   }
+const foo1 = () => {
+    console.log(100200)
+}
+const foo2 = (num: number) => {
+    console.log(num)
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/*<button onClick={() => {console.log("Hello Roma!") }}>MyYouTubeChannel</button>*/}
+
+     {/*<button onClick={(e:MouseEvent<HTMLButtonElement>) => onClickHandler("Roma")}>MyYouTubeChannel-1</button>*/}
+     {/*<button onClick={(e:MouseEvent<HTMLButtonElement>) => onClickHandler("Ivan")}>MyYouTubeChannel-2</button>*/}
+     {/*<button onClick={() => onClickHandler("some info")}>MyYouTubeChannel-3</button>*/}
+        <button onClick={foo1}>1</button>
+        <button onClick={() => foo2(100200)}>2</button>
     </div>
   );
 }
