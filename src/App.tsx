0987a02ea0cd93d07abdd5991b7e21836dd477de
digitrom <roma.bobrovski@gmail.com>
2023-01-29 22:40:02 +0300

    Super Button - finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,17 @@
-import React, {MouseEvent} from 'react';
+import React from 'react';
 import './App.css';
-
+import Button from "./Button";
 
 
 function App() {
-  // const myFirstSubscriber = (e:MouseEvent<HTMLButtonElement>) => {
-  //     console.log("Hello Roma!") }
-  //   const mySecondSubscriber = (e:MouseEvent<HTMLButtonElement>) => {
-  //     console.log("Hello Ivan!") }
-  //   const names = [
-  //       { name1: "Roma", name2: "Ivan"}
-  //   ]
-  //
-  //   const onClickHandler = (name: string) => {
-  //        console.log(name)
-  //   }
-const foo1 = () => {
-    console.log(100200)
-}
-const foo2 = (num: number) => {
-    console.log(num)
-}
-
-
-  return (
-    <div className="App">
-     {/*<button onClick={() => {console.log("Hello Roma!") }}>MyYouTubeChannel</button>*/}
-
-     {/*<button onClick={(e:MouseEvent<HTMLButtonElement>) => onClickHandler("Roma")}>MyYouTubeChannel-1</button>*/}
-     {/*<button onClick={(e:MouseEvent<HTMLButtonElement>) => onClickHandler("Ivan")}>MyYouTubeChannel-2</button>*/}
-     {/*<button onClick={() => onClickHandler("some info")}>MyYouTubeChannel-3</button>*/}
-        <button onClick={foo1}>1</button>
-        <button onClick={() => foo2(100200)}>2</button>
-    </div>
-  );
+    const func1 = (name:string, age: number) => {
+    console.log(name, age)
+    }
+    return (
+        <div className="App">
+            <Button name={"Roman"} callBack={() => func1("Roman", 37)}/>
+        </div>
+    );
 }
 
 export default App;
